e29a2c9ce6ceae1b081d0807e123edb79580022c
Sanika Kotalwar <146529091+Sanikabk@users.noreply.github.com>
2025-08-24 23:48:52 +0530

    Create Day10.java
    Problem : Group Anagrams
    You are given an array of strings strs[]. Your task is to group all the strings that are anagrams of each other. An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
    The goal is to return the grouped anagrams as a list of lists, where each sublist contains words that are anagrams of each other.

---
+++ Day10.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] strs = new String[n];
+        for (int i = 0; i < n; i++) {
+            strs[i] = sc.nextLine();
+        }
+        List<List<String>> result = groupAnagrams(strs);
+        System.out.println(result);
+        sc.close();
+    }
+}
