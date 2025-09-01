3aee50b3f7b75fa7006ee4540f8f8352778ad497
Sanika Kotalwar <146529091+Sanikabk@users.noreply.github.com>
2025-09-01 13:35:48 +0530

    Create Day18.java
    *Day 18 of 30* 🗓️
    
    *Problem* : Count the Number of Divisors of a Number
    Given a positive integer N, your task is to find the total number of divisors (factors) of N. A divisor of N is any integer that divides N without leaving a remainder.
    A divisor of a number is any integer that divides the number evenly (i.e., without a remainder). For example, for N = 12, its divisors are 1, 2, 3, 4, 6, 12, so the total number of divisors is 6.

---
+++ Day18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        int count = 0;
+        
+        for (long i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count += (i * i == n) ? 1 : 2;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
